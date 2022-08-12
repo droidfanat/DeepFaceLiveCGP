26dbd2bdbc1e8ccb72a1cd2db64ea6f895bce6df
Erik <xdoom88@gmail.com>
2022-08-12 18:18:24 +0300

    add sream

--- apps/DeepFaceLive/console.py
+++ apps/DeepFaceLive/console.py
@@ -475,7 +475,7 @@ class FaceMerger():
         self.interpolation = 'bilinear'
         self.color_compression = 1.0
         self.face_opacity = 1.0
-        self.device = "CPU"
+        self.device = "GPU"
 
     _cpu_interp = {'bilinear' : ImageProcessor.Interpolation.LINEAR,
                    'bicubic'  : ImageProcessor.Interpolation.CUBIC,
